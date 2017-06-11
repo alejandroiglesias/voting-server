287042313ad8e83db46770bff41b47fe8d50dd9f
Alejandro Iglesias <ale.garciaiglesias@gmail.com>
2017-06-11 19:56:31 -0300

    Resolve excercise #3

--- test/core_spec.js
+++ test/core_spec.js
@@ -110,13 +110,18 @@ describe('application logic', () => {
 
     it('creates a tally for the voted entry', () => {
       const state = Map({
+        round: 1,
         pair: List.of('Trainspotting', '28 Days Later')
       });
-      const nextState = vote(state, 'Trainspotting');
+      const nextState = vote(state, 'Trainspotting', 'voter1');
       expect(nextState).to.equal(Map({
+        round: 1,
         pair: List.of('Trainspotting', '28 Days Later'),
         tally: Map({
           'Trainspotting': 1
+        }),
+        votes: Map({
+          voter1: 'Trainspotting'
         })
       }));
     });
@@ -127,14 +132,44 @@ describe('application logic', () => {
         tally: Map({
           'Trainspotting': 3,
           '28 Days Later': 2
-        })
+        }),
+        votes: Map()
       });
-      const nextState = vote(state, 'Trainspotting');
+      const nextState = vote(state, 'Trainspotting', 'voter1');
       expect(nextState).to.equal(Map({
         pair: List.of('Trainspotting', '28 Days Later'),
         tally: Map({
           'Trainspotting': 4,
           '28 Days Later': 2
+        }),
+        votes: Map({
+          voter1: 'Trainspotting'
+        })
+      }));
+    });
+
+    it('nullifies previous vote for the same voter', () => {
+      const state = Map({
+        round: 1,
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 3,
+          '28 Days Later': 2
+        }),
+        votes: Map({
+          voter1: '28 Days Later'
+        })
+      });
+      const nextState = vote(state, 'Trainspotting', 'voter1');
+      expect(nextState).to.equal(Map({
+        round: 1,
+        pair: List.of('Trainspotting', '28 Days Later'),
+        tally: Map({
+          'Trainspotting': 4,
+          '28 Days Later': 1
+        }),
+        votes: Map({
+          voter1: 'Trainspotting'
         })
       }));
     });
